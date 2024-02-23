2e0ed61143c2195c5fb53e896072877f50617b77
Nelson Dario Florez Ceron <46226444+nelsonflorez11@users.noreply.github.com>
2024-02-22 21:54:22 -0500

    Create parametrizadoDSL03.groovy
    add

---
+++ parametrizadoDSL03.groovy
@@ -0,0 +1,26 @@
+job('hijo02-dsl') {
+	description('Job DSL de ejemplo para el curso de Jenkins')
+  	scm {
+      		git('https://github.com/nelsonflorez11/jenkins.job.parametrizado.git', 'main') { node ->
+        		node / gitConfigName('nelsonflorez11')
+        		node / gitConfigEmail('nelsonflorez11@gmail.com')
+      		}
+    	} 
+  		parameters {
+   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+      		booleanParam('agente', false)
+    	}
+  		triggers {
+    		cron('H/7 * * * *')
+    	}
+  		steps {
+    		shell("bash jobscript.sh")
+    	}
+  		 publishers {
+      		mailer('nelsonflorez11@gmail.com', true, true)
+    	}
+  
+
+    	
+}
