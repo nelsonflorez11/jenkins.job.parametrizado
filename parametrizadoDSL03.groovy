18d6e2e7c6a8d88a01e1bb14dc6a4e2908b49307
Nelson Dario Florez Ceron <46226444+nelsonflorez11@users.noreply.github.com>
2024-02-27 23:01:36 -0500

    Update parametrizadoDSL03.groovy

--- parametrizadoDSL03.groovy
+++ parametrizadoDSL03.groovy
@@ -1,5 +1,5 @@
 job('hijo02-dsl') {
-	description('gano santafe')
+	description('gano santafe al chico')
   	scm {
       		git('https://github.com/nelsonflorez11/jenkins.job.parametrizado.git', 'main') { node ->
         		node / gitConfigName('nelsonflorez11')
